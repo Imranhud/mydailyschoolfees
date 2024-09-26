9da3c09d3fb3cdc362ecd2067f61485970d9e243
Imran Hud <imranhud26@gmail.com>
2024-09-27 00:46:55 +0100

    app3

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,10 +5,10 @@ const NavBar = () => {
   return (
     <div className='flex justify-between items-center'>
         <div className="flex flex-row mt-2">
-            <img src={img0} className='h-[55px] w-[55px] mx-3 rounded-full' alt="" />
-            <div className="flex flex-col">
+            <img src={img0} className='h-[55px] w-[55px] mx-2 rounded-full' alt="" />
+            <div className="flex flex-col flex-1">
                 <h3 className='text-white sm:text-[25px] text-[20px] font-poppins '>Imranhud</h3>
-                <p className='text-dimWhite text-[15px] font-poppins'>Bsc Biochemistry</p>
+                <p className='text-dimWhite text-[15px] font-poppins'>Bsc. Biochemistry</p>
             </div>
         </div>
         <div className="flex flex-row items-center justify-between mr-3">
